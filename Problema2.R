0d2a6fa4bd5bc8d2808f6a8b332f6a08304f904a
marc.merino.lopez <marc.merino.lopez@auleseebe.upc.edu>
2025-02-13 13:44:59 +0100

    zz

--- Problema2.R
+++ Problema2.R
@@ -1,1 +1,60 @@
 #Codigo para problema 2
+
+
+#tabla frequencias
+
+data<-mtcars
+data
+
+x <- data$cyl
+ni <- table(x)
+ni
+
+# length(ni)
+#[1] 3
+#> names(ni)
+#[1] "4" "6" "8"
+fi<-ni/length(x)
+fi
+Ni<-cumsum(ni)
+Ni
+Fi<-cumsum(fi)
+Fi
+cbind(ni, fi, Ni, Fi)
+
+
+#continuos
+x<-data$mpg
+bins<-cut(x,10)
+bins
+
+
+
+#discretas
+x <- data$cyl
+ni <- table(x)
+fi<-ni/length(x)
+Ni<-cumsum(ni)
+Fi<-cumsum(fi)
+cbind(ni, fi, Ni, Fi)
+
+#continuos
+x<-data$mpg
+bins<-cut(x,10)
+x <- data$cyl
+ni <- table(x)
+fi<-ni/length(x)
+Ni<-cumsum(ni)
+Fi<-cumsum(fi)
+cbind(ni, fi, Ni, Fi)
+
+#histograma: continuas
+hist(data$mpg, breaks=10)
+
+#histograma: discretas
+ni<-table(data$cyl)
+barplot(ni)
+
+#pie: discretas
+fi<-ni/length(data$cyl)
+pie(fi)
--- Problema3.R
+++ Problema3.R
@@ -1,1 +1,10 @@
 #Codigo para problema 3
+#sample(0:10, 10, replace=TRUE)
+#x<-(1,3,5---,10)    y<-eliminando()
+x<-[-8,5,2,-8,9,5,2,-3,1,-1,4,-4,9,3,-9,7,0,-7,8,-4,1,7,-6,4,5,-9,-2,-8,5,-5]
+sum(x)
+y<-x[-c(3,7,8)]
+sum(exp(x))-sum(exp(y))
+y[5]
+y[2]
+
